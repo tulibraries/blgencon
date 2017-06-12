42df76784719273f69f2eaaf9fdf6c72eb41e924
cdoyle-temple <cdoyle@temple.edu>
2017-06-12 16:25:37 -0400

    Added google analytics to production environment. (#9)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Google Analytics tracking info
+  GA.tracker = "UA-2152985-16"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
